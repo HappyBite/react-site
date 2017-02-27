c933926354a5f06840a6ec5b096e88374675d37a
cloudpen <lantzgabriel@gmail.com>
2017-02-27 22:31:30 +0000

    Committing

--- data-store/twixly-client.js
+++ data-store/twixly-client.js
@@ -1,7 +1,7 @@
 var config = require('../config');
 var twixly = require('twixly');
 
-var client = new twixly.Client({
+var client = new twixly.createClient({
   bucket: config.BUCKET_ID,
   accessToken: config.ACCESS_TOKEN
 });
